d8ea45fd1ee5267090eb30913b2978a00a2536b9
jahu1251 <56964174+jahu1251@users.noreply.github.com>
2020-11-25 19:50:31 +0100

    Add files via upload

--- search_func_nam.cpp
+++ search_func_nam.cpp
@@ -5,37 +5,67 @@
 #include<fstream>
 #include<string>
 #include<list>
-#include <regex>
+#include<regex>
 
 
-std::map<std::string, std::vector<std::pair<std::string,double>>> wyszukiwanie2(std::map<std::string, double> pliki) {
+std::map<std::string, std::vector<std::string>> wyszukiwanie2(std::map<std::string, double> pliki) {
 
-    int counter = 0;
-    std::map<std::string, std::vector<std::pair<std::string,double>>>  mapaa;
+    std::map<std::string, std::vector<std::string>>  function_map;
     std::regex rgx("\\b\\w+[A-Za-z0-9_]+(?=\\()");
     std::regex rgx2("(\\.|\\:)\\b\\w+[A-Za-z0-9_]+(?=\\()");
     std::smatch match;
     std::smatch match2;
+    std::string key;
+    std::string found;
 
     for (auto it = pliki.begin(); it != pliki.end(); ++it) {
-    std::ifstream plik(it->first);
 
-    while(!plik.eof()){
-        
-        std::string linijka;
-        getline(plik, linijka);
+        int counter2 = 0;
+        int counter = 0;
+        std::ifstream plik(it->first);
+        while(!plik.eof()){
+           
+            std::string linijka;
+            getline(plik, linijka);
 
-        if (std::regex_search(linijka, match, rgx) && !(std::regex_search(linijka, match2, rgx2))){
-            counter++;
-            std::cout << "funkcja " << counter << " " << match[0] << '\n';
 
+            if (std::regex_search(linijka, match, rgx) && !(std::regex_search(linijka, match2, rgx2))){
+
+                if (match[0] != "_popen" && match[0] != "popen" && match[0] != "_pclose" && match[0] != "pclose" && match[0] != "file" && match[0] != "if" && match[0] != "for" && match[0] != "graf_plikow" && match[0] != "graf_funkcji" && match[0] != "graf_namespace" && match[0] != "ifile" && match[0] != "ofile" && match[0] != "cfile" && match[0] != "rgb" && match[0] != "while" && match[0] != "rgx" && match[0] != "rgx2" && match[0] != "plik" && match[0] != "getline" && match[0] != "fgets" && match[0] != "DODAWANIE" && match[0] != "MAPA"){
+                    
+                    found = match[0];
+
+                    if(counter == 0){
+                        key = found;
+                    }
+
+                    if(found != key){
+                        function_map[key].push_back(found);
+                    }
+                    else if(found == key){
+                        function_map[key].push_back("");
+                    }
+
+                }
+            }
+
+
+            if(linijka.find("{") != std::string::npos && linijka.find("class") == std::string::npos){
+                ++counter;
+                std::cout << "counter wynosi " << counter << " w " << found << std::endl; 
+            }
+
+            if(linijka.find("}") != std::string::npos){
+                --counter;
+                std::cout << "counter wynosi " << counter << " w " << found << std::endl;
+                
+            }
+
+            counter2++;
         }
-    }
+        }
 
-        // mapaa[it->first].push_back(std::make_pair(nazwa_pliku,it->second));         
-    }
-     
-    return mapaa;
+    return function_map;
     
 }
 
